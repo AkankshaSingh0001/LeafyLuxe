e32d352a3ba8214e5b6e9d8c1c39064832867ffd
Akanksha Singh <133987648+AkankshaSingh0001@users.noreply.github.com>
2024-08-01 22:25:30 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -104,7 +104,7 @@ function App() {
   axios.default.withCredentials=true;
 const handleSumbit=(e)=>{
   e.preventDefault();
-  axios.post('https://leafy-luxe.vercel.app/register',{name,email,password})
+  axios.post('https://leafy-luxe.vercel.app/')
   .then(result=>console.log(result))
   .catch(err=>console.log(err))
 }
