00e13930a1e0e27acd803936e9ff13dfcd7ac309
Akanksha Singh <133987648+AkankshaSingh0001@users.noreply.github.com>
2024-08-01 22:36:24 +0530

    Update App.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -99,15 +99,15 @@ import Cancle from './Components_admin/Cancle';
 
 function App() {
 
-const handleSumbit=(e)=>{axios.post('https://leafy-luxe.vercel.app/')}
+// const handleSumbit=(e)=>{axios.post('https://leafy-luxe.vercel.app/')}
 
-//   axios.default.withCredentials=true;
-// const handleSumbit=(e)=>{
-//   e.preventDefault();
+  axios.default.withCredentials=true;
+const handleSumbit=(e)=>{
+  e.preventDefault();
   
-//   .then(result=>console.log(result))
-//   .catch(err=>console.log(err))
-// }
+  .then(result=>console.log(result))
+  .catch(err=>console.log(err))
+}
   
   return ( 
     <>
